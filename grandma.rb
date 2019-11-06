dc03d156ceffb966cbc9297f30c687411a52bb7c
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-11-06 11:39:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase == "".upcase
+  if phrase == true.upcase
     "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN"
